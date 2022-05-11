71561221b2a056c2445e504d110ef1b4ab349a09
Daniel Lee <sdlee095@gmail.com>
2022-05-11 10:28:14 -0400

    made the map work

--- script.js
+++ script.js
@@ -1,10 +1,31 @@
-window.initMap = function () {
+const apiKey = "AIzaSyDwFz89HsDDFl_dGv9Y04Hi41jBGrli2ak";
+
+window.initMap = function (latitude, longitude) {
   const options = {
-    zoom: 8,
-    center: { lat: 50.753746, lng: -90.38633 },
+    zoom: 10,
+    center: { lat: latitude, lng: longitude },
   };
   const map = new google.maps.Map(document.getElementById("map"), options);
 };
+
+const userInput = document.querySelector(".user-input");
+const userBtn = document.querySelector(".user-btn");
+const inputForm = document.querySelector(".input-form")
+inputForm.addEventListener("submit", (e) => {
+  e.preventDefault()
+  const userValue = userInput.value;
+  const urlEncodedUserValue = encodeURIComponent(userValue)
+  fetch(
+    `https://maps.googleapis.com/maps/api/geocode/json?address=${urlEncodedUserValue}&key=${apiKey}`
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      latitude = data.results[0].geometry.location.lat;
+      longitude = data.results[0].geometry.location.lng;
+      console.log(latitude, longitude);
+      initMap(latitude, longitude);
+    });
+});
 
 // var autocomplete = new google.maps.places.Autocomplete(input);
 // autocomplete.bindTo('bounds', map);
@@ -23,10 +44,3 @@ window.initMap = function () {
 //   window.alert("Autocomplete's returned place contains no geometry");
 //   return;
 // }})
-
-
-// const userInput = document.querySelector(".user-input");
-// const userBtn = document.querySelector(".user-btn");
-// userBtn.addEventListener("submit", (e) => {
-//   const userValue = userInput.value;
-// });
